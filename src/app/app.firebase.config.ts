70e1eccca1e1de838f917dfeb2f6be0fb802bb6a
Tauqeer1 <tauqeer.shakir@yahoo.com>
2018-01-13 18:33:39 +0500

    configure firebase

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyBQHirYqt_MoQ3K43E-jVcyBl1fTTpCCqg",
+  authDomain: "fir-chat-6c7cd.firebaseapp.com",
+  databaseURL: "https://fir-chat-6c7cd.firebaseio.com",
+  projectId: "fir-chat-6c7cd",
+  storageBucket: "fir-chat-6c7cd.appspot.com",
+  messagingSenderId: "308972018321"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { config } from './app.firebase.config';
+
 import { MyApp } from './app.component';
 
 @NgModule({
@@ -21,7 +25,7 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
