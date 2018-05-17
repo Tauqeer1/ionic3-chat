dfc2f44f51fe4c42dd48b55b5fecf83070166ea3
Tauqeer1 <tauqeer.shakir@yahoo.com>
2018-05-17 20:46:05 +0500

    working on chat between two person

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 import { ChatProvider } from '../../providers/chat/chat';
+import firebase from 'firebase';
 
 
 @IonicPage()
@@ -12,11 +13,20 @@ export class ChatPage {
 
   buddy: any;
   newMessage;
-  buddyMessage = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams, private chatProvider: ChatProvider) {
+  allMessages = [];
+  photoURL;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private chatProvider: ChatProvider, private events: Events) {
     this.buddy = this.chatProvider.buddy;
+    this.photoURL = firebase.auth().currentUser.photoURL
+    this.events.subscribe('newmessage', () => {
+      this.allMessages = [];
+      this.allMessages = this.chatProvider.buddyMessages;
+    });
   }
 
+  ionViewDidEnter() {
+    this.chatProvider.getBuddyMessages();
+  }
   ionViewDidLoad() {
   }
 
@@ -30,13 +40,9 @@ export class ChatPage {
         this.newMessage = '';
       }).catch(err => {
         console.error('err', err);
-      })l;
+      });
   }
 
-  gotBuddyMessages() {
-    this.buddyMessage = [];
-    this.chats.child(firebase.auth().currentUser.uiid);
-  }
 
 
 }
--- src/providers/chat/chat.ts
+++ src/providers/chat/chat.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import firebase from 'firebase';
+import { Events } from 'ionic-angular';
 
 @Injectable()
 export class ChatProvider {
 
   chats = firebase.database().ref('/chats');
   buddy: any;
+  buddyMessages = [];
 
-  constructor() {
+  constructor(private events: Events) {
 
   }
 
@@ -22,12 +24,12 @@ export class ChatProvider {
           .push({
             sentBy: firebase.auth().currentUser.uid,
             message: msg,
-            timestamp: firebase.database.ServerValue.TIMESTAMP;
+            timestamp: firebase.database.ServerValue.TIMESTAMP
           }).then(() => {
             this.chats.child(this.buddy.uid)
               .child(firebase.auth().currentUser.uid)
               .push({
-                sentBy: firebase.auth().currentUser.uid;
+                sentBy: firebase.auth().currentUser.uid,
                 message: msg,
                 timestamp: firebase.database.ServerValue.TIMESTAMP
               }).then(() => {
@@ -38,5 +40,18 @@ export class ChatProvider {
         // reject(err);
       })
     }
+  }
+
+  getBuddyMessages() {
+    let temp;
+    this.chats.child(firebase.auth().currentUser.uid).child(this.buddy.uid)
+      .on('value', (snapshot) => {
+        this.buddyMessages = [];
+        temp = snapshot.val();
+        for (let tempKey in temp) {
+          this.buddyMessages.push(temp[tempKey]);
+        }
+        this.events.publish('newmessage');
+      });
   }
 }
